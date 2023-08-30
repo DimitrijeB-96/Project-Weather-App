bc109afdb31a2e8add6565f705cf34b797b4fff1
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-08-30 09:52:52 +0200

    Setup completed

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+// Make Novi Sad default location to load on page load
+// Input for searching location
+// Toggle Fahrenheit-Celsius
+// Make background color-picture respond to weather condition
+// add Giphy ?
+
