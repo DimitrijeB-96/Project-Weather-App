cf2471ca63c42b0137d1ec3dfc3eda26413b4eb1
DimitrijeB-96 <mitabackalic96@gmail.com>
2023-08-30 10:07:56 +0200

    Models added, project will be done by implementing MVC pattern

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './style.css';
 
+import { Controller } from './controller.js';
+import { Model } from './model.js';
+import { View } from './view.js';
+
 // Make Novi Sad default location to load on page load
 // Input for searching location
 // Toggle Fahrenheit-Celsius
 // Make background color-picture respond to weather condition
 // add Giphy ?
+
+const app = new Controller(new Model(), new View());
+
 
